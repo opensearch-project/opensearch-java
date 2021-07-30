31c0e063af8de92ca323a649c3c92f2ddcb621fe
Mital Awachat <mitalawachat@users.noreply.github.com>
2021-07-30 10:12:28 +0530

    Remove xpack related code (#4)
    Signed-off-by: Mital Awachat <mitalawachat@users.noreply.github.com>

--- java-client/src/main/java/co/elastic/clients/elasticsearch/nodes/info/NodeInfoSettings.java
+++ java-client/src/main/java/co/elastic/clients/elasticsearch/nodes/info/NodeInfoSettings.java
@@ -75,9 +75,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 	private final NodeInfoSettingsNetwork network;
 
 	@Nullable
-	private final NodeInfoXpack xpack;
-
-	@Nullable
 	private final NodeInfoScript script;
 
 	@Nullable
@@ -101,7 +98,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 		this.bootstrap = builder.bootstrap;
 		this.transport = ApiTypeHelper.requireNonNull(builder.transport, this, "transport");
 		this.network = builder.network;
-		this.xpack = builder.xpack;
 		this.script = builder.script;
 		this.search = builder.search;
 		this.ingest = builder.ingest;
@@ -195,14 +191,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 	}
 
 	/**
-	 * API name: {@code xpack}
-	 */
-	@Nullable
-	public final NodeInfoXpack xpack() {
-		return this.xpack;
-	}
-
-	/**
 	 * API name: {@code script}
 	 */
 	@Nullable
@@ -280,11 +268,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 			this.network.serialize(generator, mapper);
 
 		}
-		if (this.xpack != null) {
-			generator.writeKey("xpack");
-			this.xpack.serialize(generator, mapper);
-
-		}
 		if (this.script != null) {
 			generator.writeKey("script");
 			this.script.serialize(generator, mapper);
@@ -336,9 +319,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 
 		@Nullable
 		private NodeInfoSettingsNetwork network;
-
-		@Nullable
-		private NodeInfoXpack xpack;
 
 		@Nullable
 		private NodeInfoScript script;
@@ -519,21 +499,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 		}
 
 		/**
-		 * API name: {@code xpack}
-		 */
-		public final Builder xpack(@Nullable NodeInfoXpack value) {
-			this.xpack = value;
-			return this;
-		}
-
-		/**
-		 * API name: {@code xpack}
-		 */
-		public final Builder xpack(Function<NodeInfoXpack.Builder, ObjectBuilder<NodeInfoXpack>> fn) {
-			return this.xpack(fn.apply(new NodeInfoXpack.Builder()).build());
-		}
-
-		/**
 		 * API name: {@code script}
 		 */
 		public final Builder script(@Nullable NodeInfoScript value) {
@@ -613,7 +578,6 @@ public class NodeInfoSettings implements JsonpSerializable {
 		op.add(Builder::bootstrap, NodeInfoBootstrap._DESERIALIZER, "bootstrap");
 		op.add(Builder::transport, NodeInfoSettingsTransport._DESERIALIZER, "transport");
 		op.add(Builder::network, NodeInfoSettingsNetwork._DESERIALIZER, "network");
-		op.add(Builder::xpack, NodeInfoXpack._DESERIALIZER, "xpack");
 		op.add(Builder::script, NodeInfoScript._DESERIALIZER, "script");
 		op.add(Builder::search, NodeInfoSearch._DESERIALIZER, "search");
 		op.add(Builder::ingest, NodeInfoSettingsIngest._DESERIALIZER, "ingest");
