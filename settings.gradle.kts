dfb039ff770685185a6524849d5dbc348c40b5bc
Mital Awachat <mitalawachat@users.noreply.github.com>
2021-08-04 08:44:52 +0530

    Remove elastic documentation reference and update licence header (#10)
    * Remove elastic documentation reference
    
    * Update licence headers
    
    Signed-off-by: Mital Awachat <mitalawachat@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,12 @@
 /*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
  * Licensed to Elasticsearch B.V. under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright
@@ -17,5 +25,10 @@
  * under the License.
  */
 
-rootProject.name = "elasticsearch-java"
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+rootProject.name = "opensearch-java"
 include("java-client")
