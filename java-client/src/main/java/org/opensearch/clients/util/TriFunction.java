5b6fbd433065343127227f194525ce510be398e5
Mital Awachat <mitalawachat@users.noreply.github.com>
2021-08-04 08:44:52 +0530

    Remove elastic documentation reference and update licence header (#10)
    * Remove elastic documentation reference
    
    * Update licence headers
    
    Signed-off-by: Mital Awachat <mitalawachat@users.noreply.github.com>

--- java-client/src/main/java/org/opensearch/clients/util/TriFunction.java
+++ java-client/src/main/java/org/opensearch/clients/util/TriFunction.java
@@ -1,4 +1,12 @@
 /*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
  * Licensed to Elasticsearch B.V. under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright
@@ -17,6 +25,11 @@
  * under the License.
  */
 
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.clients.util;
 
 /**
@@ -28,8 +41,6 @@ package org.opensearch.clients.util;
  * @param <V> the type of the third argument to the function
  * @param <R> the type of the result of the function
  *
- * @see java.util.function.Function
- * @see java.util.function.BiFunction
  */
 @FunctionalInterface
 public interface TriFunction<T, U, V, R> {
