cf8053ad95d1fb13309432168a269d3b6daf4726
Mital Awachat <mitalawachat@users.noreply.github.com>
2021-08-26 18:47:21 +0530

    Run security-enabled integration tests (#31)
    Co-authored-by: Mital Awachat <awachatm@amazon.com>
    
    Signed-off-by: Mital Awachat <mitalawachat@users.noreply.github.com>

--- java-client/src/test/java/org/opensearch/clients/opensearch/integTest/OpenSearchRestHighLevelClientTestCase.java
+++ java-client/src/test/java/org/opensearch/clients/opensearch/integTest/OpenSearchRestHighLevelClientTestCase.java
@@ -1,23 +1,29 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-
-package org.opensearch.clients.opensearch.integTest;
-
-
-import org.opensearch.clients.json.jackson.JacksonJsonpMapper;
-import org.opensearch.clients.opensearch.OpenSearchClient;
-import org.junit.AfterClass;
-import org.junit.Before;
-
-import java.io.IOException;
-
+///*
+// * SPDX-License-Identifier: Apache-2.0
+// *
+// * The OpenSearch Contributors require contributions made to
+// * this file be licensed under the Apache-2.0 license or a
+// * compatible open source license.
+// */
+//
+//package org.opensearch.clients.opensearch.integTest;
+//
+//
+//import org.opensearch.clients.json.jackson.JacksonJsonpMapper;
+//import org.opensearch.clients.opensearch.OpenSearchClient;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//
+//import java.io.IOException;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Optional;
+//import java.util.stream.Collectors;
+//
+//
 //public abstract class OpenSearchRestHighLevelClientTestCase extends OpenSearchRestTestCase {
-
+//
 //    private static RestClient restClient;
 //    private static OpenSearchClient restHighLevelClient;
 //
@@ -34,6 +40,52 @@ import java.io.IOException;
 //        }
 //    }
 //
+//    private boolean isHttps() {
+//        return Optional.ofNullable(System.getProperty("https"))
+//            .map("true"::equalsIgnoreCase)
+//            .orElse(false);
+//    }
+//
+//    @Override
+//    protected String getProtocol() {
+//        return isHttps() ? "https" : "http";
+//    }
+//
+//    protected static OpenSearchClient highLevelClient() {
+//        return restHighLevelClient;
+//    }
+//
+//    @Override
+//    protected RestClient buildClient(Settings settings, HttpHost[] hosts) throws IOException {
+//        RestClientBuilder builder = RestClient.builder(hosts);
+//        builder.setStrictDeprecationMode(true);
+//        if (isHttps()) {
+//            configureHttpsClient(builder);
+//        }
+//        return builder.build();
+//    }
+//
+//    private void configureHttpsClient(RestClientBuilder builder) {
+//        builder.setHttpClientConfigCallback(httpClientBuilder -> {
+//            String userName = Optional.ofNullable(System.getProperty("user")).orElse("admin");
+//            String password = Optional.ofNullable(System.getProperty("password")).orElse("admin");
+//
+//            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+//
+//            try {
+//                return httpClientBuilder
+//                    .setDefaultCredentialsProvider(credentialsProvider)
+//                    // disable the certificate since our testing cluster just uses the default security configuration
+//                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+//                    .setSSLContext(SSLContextBuilder.create().loadTrustMaterial(null, (chains, authType) -> true).build());
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+//    }
+//
+//    @Override
 //    protected String getTestRestCluster() {
 //        String cluster = System.getProperty("tests.rest.cluster");
 //        if (cluster == null) {
@@ -52,7 +104,40 @@ import java.io.IOException;
 //        }
 //    }
 //
-//    protected static OpenSearchClient highLevelClient() {
-//        return restHighLevelClient;
+//    @SuppressWarnings("unchecked")
+//    @After
+//    protected void wipeAllOSIndices() throws IOException {
+//        Response response = adminClient().performRequest(new Request("GET", "/_cat/indices?format=json&expand_wildcards=all"));
+//        XContentType xContentType = XContentType.fromMediaTypeOrFormat(response.getEntity().getContentType().getValue());
+//        XContentParser parser = xContentType.xContent().createParser(
+//            NamedXContentRegistry.EMPTY, DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
+//            response.getEntity().getContent());
+//
+//        try {
+//            XContentParser.Token token = parser.nextToken();
+//            List<Map<String, Object>> parserList;
+//            if (token == XContentParser.Token.START_ARRAY) {
+//                parserList = parser.listOrderedMap().stream().map(obj -> (Map<String, Object>) obj).collect(Collectors.toList());
+//            } else {
+//                parserList = Collections.singletonList(parser.mapOrdered());
+//            }
+//
+//            for (Map<String, Object> index : parserList) {
+//                String indexName = (String) index.get("index");
+//                if (indexName != null && !".opendistro_security".equals(indexName)) {
+//                    adminClient().performRequest(new Request("DELETE", "/" + indexName));
+//                }
+//            }
+//        } finally {
+//            parser.close();
+//        }
+//    }
+//
+//    /**
+//     * wipeAllIndices won't work since it cannot delete security index. Use wipeAllOSIndices instead.
+//     */
+//    @Override
+//    protected boolean preserveIndicesUponCompletion() {
+//        return true;
 //    }
 //}
