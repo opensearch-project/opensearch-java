680e6dff971b6a9517edcdcbace885c5a41dd4d7
Rishab Nahata <rnnahata@amazon.com>
2021-11-18 15:30:51 +0530

    Use try-with-resource for input stream in RestClientTransport
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- java-client/src/main/java/org/opensearch/clients/transport/rest_client/RestClientTransport.java
+++ java-client/src/main/java/org/opensearch/clients/transport/rest_client/RestClientTransport.java
@@ -250,9 +250,10 @@ public class RestClientTransport implements Transport {
                 JsonpDeserializer<ResponseT> responseParser = endpoint.responseParser();
                 if (responseParser != null) {
                     // Expecting a body
-                    InputStream content = clientResp.getEntity().getContent();
-                    JsonParser parser = mapper.jsonProvider().createParser(content);
-                    response = responseParser.deserialize(parser, mapper);
+                    try (InputStream content = clientResp.getEntity().getContent()) {
+                        JsonParser parser = mapper.jsonProvider().createParser(content);
+                        response = responseParser.deserialize(parser, mapper);
+                    }
                 }
                 return response;
             }
