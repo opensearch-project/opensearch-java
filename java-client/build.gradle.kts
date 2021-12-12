f4cd9025c2a8cdd8c7696c5506409bc61ab05717
Rishab Nahata <rishabnahata07@gmail.com>
2021-12-12 19:51:11 +0530

    Bump Opensearch version to 1.2.1 (#70)
    * Bump Opensearch version to 1.2.1
    
    Signed-off-by: Rishab Nahata <rnnahata@amazon.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -37,6 +37,7 @@ buildscript {
     repositories {
         mavenCentral()
         mavenLocal()
+        maven(url = "https://aws.oss.sonatype.org/content/repositories/snapshots/")
         maven(url = "https://plugins.gradle.org/m2/")
         jcenter()
     }
@@ -114,7 +115,7 @@ val integrationTest = task<Test>("integrationTest") {
 }
 
 dependencies {
-    val opensearchVersion = "1.0.0"
+    val opensearchVersion = "1.2.1"
     val jacksonVersion = "2.12.5"
 
     // Apache 2.0
