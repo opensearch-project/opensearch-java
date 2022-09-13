38b562d96c6881b8ff970480b07c2a056b285347
Vacha Shah <vachshah@amazon.com>
2022-09-13 21:06:28 +0000

    Upgrading to the latest dependency-license-report in its 1.x line
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -48,7 +48,7 @@ plugins {
     `java-library`
     checkstyle
     `maven-publish`
-    id("com.github.jk1.dependency-license-report") version "1.17"
+    id("com.github.jk1.dependency-license-report") version "1.19"
 }
 
 checkstyle {
