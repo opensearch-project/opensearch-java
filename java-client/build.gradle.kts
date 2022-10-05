33c4a71d8bfff8b92f44dd099a777381f37fdd2e
Vacha Shah <vachshah@amazon.com>
2022-10-05 09:58:07 -0700

    Removing strong dependency for jakarta-json-api (#221)
    Signed-off-by: Vacha Shah <vachshah@amazon.com>
    
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -143,10 +143,6 @@ dependencies {
     // https://search.maven.org/artifact/com.google.code.findbugs/jsr305
     api("com.google.code.findbugs:jsr305:3.0.2")
 
-    // EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
-    // https://github.com/eclipse-ee4j/jsonp
-    api("jakarta.json:jakarta.json-api:2.0.1")
-
     // Needed even if using Jackson to have an implementation of the Jsonp object model
     // EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
     // https://github.com/eclipse-ee4j/parsson
