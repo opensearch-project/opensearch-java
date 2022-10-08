974931728aface7e6f514f63bf8cbb51b673efbd
AarjavP <aarjavpatel@gmail.com>
2022-10-08 16:22:22 -0400

    Remove "THIS CODE IS GENERATED" comments (#218)
    Signed-off-by: AarjavP <aarjavpatel@gmail.com>

--- java-client/src/main/java/org/opensearch/client/opensearch/core/search/SourceConfigBuilders.java
+++ java-client/src/main/java/org/opensearch/client/opensearch/core/search/SourceConfigBuilders.java
@@ -30,10 +30,6 @@
  * GitHub history for details.
  */
 
-//----------------------------------------------------
-// THIS CODE IS GENERATED. MANUAL EDITS WILL BE LOST.
-//----------------------------------------------------
-
 package org.opensearch.client.opensearch.core.search;
 
 /**
