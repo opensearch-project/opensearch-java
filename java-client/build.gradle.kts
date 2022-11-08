7f72d838c27ac3838385deb6dcf03de0b2449988
Andriy Redko <andriy.redko@aiven.io>
2022-11-08 10:11:07 -0500

    Update junit to 4.13.1
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -160,7 +160,7 @@ dependencies {
     testImplementation("io.github.classgraph:classgraph:4.8.116")
 
     // Eclipse 1.0
-    testImplementation("junit", "junit" , "4.12") {
+    testImplementation("junit", "junit" , "4.13.1") {
         exclude(group = "org.hamcrest")
     }
 }
