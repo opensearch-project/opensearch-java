9657df635dc1b5aed5dbc2e604f685f803b5091c
Andriy Redko <andriy.redko@aiven.io>
2023-01-13 16:59:15 -0500

    Introduce new OpenSearchTransport based on Apache HttpClient 5 (#281)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

---
+++ java-client/src/test/java/org/opensearch/client/opensearch/integTest/OpenSearchTransportSupport.java
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.client.opensearch.integTest;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.apache.hc.core5.http.HttpHost;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.common.settings.Settings;
+
+public interface OpenSearchTransportSupport {
+    default boolean isHttps() {
+        return Optional.ofNullable(System.getProperty("https"))
+                .map("true"::equalsIgnoreCase)
+                .orElse(false);
+    }
+
+    default OpenSearchClient buildJavaClient(Settings settings, HttpHost[] hosts) throws IOException {
+        return new OpenSearchClient(buildTransport(settings, hosts));
+    }
+
+    OpenSearchTransport buildTransport(Settings settings, HttpHost[] hosts) throws IOException;
+}
