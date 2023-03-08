88305eabd3cb46aa3f555b897e40b7a51177bfa6
Ilya Lukyanovich <ilya.lukyanovich@dataart.com>
2023-03-08 21:46:50 +0100

    Add "mappings" field to IndexSettings object (#382)
    Signed-off-by: Ilya Lukyanovich <l000kian@gmail.com>
    Signed-off-by: MarinaRazumovsky <rzm.mrn@gmail.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/experiments/ParsingTests.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/experiments/ParsingTests.java
@@ -32,14 +32,15 @@
 
 package org.opensearch.client.opensearch.experiments;
 
-import org.opensearch.client.opensearch.experiments.api.FooRequest;
-import org.opensearch.client.json.jsonb.JsonbJsonpMapper;
 import jakarta.json.spi.JsonProvider;
 import jakarta.json.stream.JsonGenerator;
 import jakarta.json.stream.JsonParser;
 import jakarta.json.stream.JsonParsingException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.opensearch.client.json.jsonb.JsonbJsonpMapper;
+import org.opensearch.client.opensearch.experiments.api.FooRequest;
+import org.opensearch.client.opensearch.indices.IndexSettingsMapping;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -55,13 +56,13 @@ public class ParsingTests extends Assert {
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
       FooRequest foo = FooRequest.builder()
-          .name("z")
-          .value(1)
-          .indices("a", "b", "c")
-          .bar(b -> b
-              .name("Raise the bar")
-          )
-          .build();
+              .name("z")
+              .value(1)
+              .indices("a", "b", "c")
+              .bar(b -> b
+                      .name("Raise the bar")
+              )
+              .build();
 
       JsonProvider provider = JsonProvider.provider();
       JsonGenerator generator = provider.createGenerator(baos);
@@ -81,6 +82,44 @@ public class ParsingTests extends Assert {
       assertNull(foo2.size());
       assertEquals(foo.indices(), foo2.indices());
       assertEquals("Raise the bar", foo.bar().name());
+    } catch (JsonParsingException je) {
+      throw new JsonParsingException(je.getMessage() + " at " + je.getLocation(), je, je.getLocation());
+    }
+  }
+
+  @Test
+  public void testIndexSettingsMappingParsing() {
+
+    try {
+
+      var baos = new ByteArrayOutputStream();
+
+      var mapping = IndexSettingsMapping.of(b -> b
+              .totalFields(d -> d.limit(1L))
+              .depth(d -> d.limit(2L))
+              .nestedFields(d -> d.limit(3L))
+              .nestedObjects(d -> d.limit(4L))
+              .fieldNameLength(d -> d.limit(5L)));
+
+      var provider = JsonProvider.provider();
+      var generator = provider.createGenerator(baos);
+      mapping.serialize(generator, new JsonbJsonpMapper());
+
+      generator.close();
+      var str = baos.toString();
+
+      assertEquals("{\"total_fields\":{\"limit\":1},\"depth\":{\"limit\":2},\"nested_fields\":{\"limit\":3}," +
+                   "\"nested_objects\":{\"limit\":4},\"field_name_length\":{\"limit\":5}}", str);
+
+      var parser = provider.createParser(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)));
+
+      var deserialized = IndexSettingsMapping._DESERIALIZER.deserialize(parser, new JsonbJsonpMapper());
+
+      assertEquals(mapping.totalFields().limit(), deserialized.totalFields().limit());
+      assertEquals(mapping.depth().limit(), deserialized.depth().limit());
+      assertEquals(mapping.nestedFields().limit(), deserialized.nestedFields().limit());
+      assertEquals(mapping.nestedObjects().limit(), deserialized.nestedObjects().limit());
+      assertEquals(mapping.fieldNameLength().limit(), deserialized.fieldNameLength().limit());
     } catch (JsonParsingException je) {
       throw new JsonParsingException(je.getMessage() + " at " + je.getLocation(), je, je.getLocation());
     }
