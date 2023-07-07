ccbe10a642797c37d718b6c1458d24b2aaadbba4
Andriy Redko <andriy.redko@aiven.io>
2023-07-07 18:54:27 -0400

    Exclude Apache HttpClient5 transitive dependencies on Apache HttpCore5 (#569)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -151,7 +151,9 @@ dependencies {
     compileOnly("org.opensearch.client", "opensearch-rest-client", opensearchVersion)
     testImplementation("org.opensearch.test", "framework", opensearchVersion)
 
-    api("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+    api("org.apache.httpcomponents.client5:httpclient5:5.2.1") {
+      exclude(group = "org.apache.httpcomponents.core5")
+    }
     api("org.apache.httpcomponents.core5:httpcore5:5.2.2")
     api("org.apache.httpcomponents.core5:httpcore5-h2:5.2.2")
 
