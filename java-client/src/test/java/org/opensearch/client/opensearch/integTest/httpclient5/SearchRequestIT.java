6f2a6ac00bb5b4239819da5dc8f7348180d7dc74
Andriy Redko <andriy.redko@aiven.io>
2023-10-20 14:22:35 -0400

    Add basic Search API integration test
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

---
+++ java-client/src/test/java/org/opensearch/client/opensearch/integTest/httpclient5/SearchRequestIT.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.client.opensearch.integTest.httpclient5;
+
+import org.opensearch.client.opensearch.integTest.AbstractSearchRequestIT;
+
+public class SearchRequestIT extends AbstractSearchRequestIT implements HttpClient5TransportSupport {}
