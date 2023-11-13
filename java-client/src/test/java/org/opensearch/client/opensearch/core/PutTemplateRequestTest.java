7744f3a55522cd9564727873848a72a8d2e3c3b5
bfindlay <bfindlay@acm.org>
2023-11-14 08:40:14 +1100

    update for pr review
    Signed-off-by: bfindlay <bfindlay@acm.org>

--- java-client/src/test/java/org/opensearch/client/opensearch/core/PutTemplateRequestTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/core/PutTemplateRequestTest.java
@@ -12,33 +12,26 @@ import org.opensearch.client.json.jsonb.JsonbJsonpMapper;
 import org.opensearch.client.opensearch.indices.PutTemplateRequest;
 
 public class PutTemplateRequestTest extends Assert {
-    private static String INDEX_NAME_FIELD = "name";
-    private static String INDEX_PATTERNS_FIELD = "index_patterns";
-    private static String CREATE_FIELD = "create";
-    private static String ORDER_FIELD = "order";
 
     @Test
     public void deserialize_validFieldsIncluded_RequestIsBuilt() throws JsonProcessingException {
-        // Arrange
         final JsonpMapper mapper = new JsonbJsonpMapper();
         final Map<String, Object> indexTemplateMap = Map.of(
-            INDEX_NAME_FIELD,
+            "name",
             "test",
-            INDEX_PATTERNS_FIELD,
+            "index_patterns",
             "*",
-            CREATE_FIELD,
+            "create",
             true,
-            ORDER_FIELD,
+            "order",
             1
 
         );
         final String indexTemplate = new ObjectMapper().writeValueAsString(indexTemplateMap);
         final var parser = mapper.jsonProvider().createParser(new StringReader(indexTemplate));
 
-        // Act
         final PutTemplateRequest putTemplateRequest = PutTemplateRequest._DESERIALIZER.deserialize(parser, mapper);
 
-        // Assert
         assertEquals(putTemplateRequest.name(), "test");
         assertEquals(putTemplateRequest.indexPatterns(), List.of("*"));
         assertEquals((long) putTemplateRequest.order(), 1L);
