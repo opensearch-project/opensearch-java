8cc25cb2039c8ad91b9f805b6765a76b53fb9a5e
bfindlay <bfindlay@acm.org>
2023-11-26 18:23:40 +1100

    fix stats null issues
    Signed-off-by: bfindlay <bfindlay@acm.org>

---
+++ java-client/src/test/java/org/opensearch/client/opensearch/integTest/httpclient5/StatsIT.java
@@ -0,0 +1,5 @@
+package org.opensearch.client.opensearch.integTest.httpclient5;
+
+import org.opensearch.client.opensearch.integTest.AbstractStatsIT;
+
+public class StatsIT extends AbstractStatsIT implements HttpClient5TransportSupport {}
