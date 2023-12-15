046e46746c331e29e6e1ce05ff4e560ac5ccece3
channel-dante <dante@channel.io>
2023-12-15 14:47:09 +0900

    fix CommonTermsQuery.toBuilder()
    Signed-off-by: channel-dante <dante@channel.io>

--- java-client/src/main/java/org/opensearch/client/opensearch/_types/query_dsl/CommonTermsQuery.java
+++ java-client/src/main/java/org/opensearch/client/opensearch/_types/query_dsl/CommonTermsQuery.java
@@ -183,7 +183,13 @@ public class CommonTermsQuery extends QueryBase implements QueryVariant {
     }
 
     public Builder toBuilder() {
-        return new Builder().field(field);
+        return new Builder().field(field)
+            .analyzer(analyzer)
+            .cutoffFrequency(cutoffFrequency)
+            .highFreqOperator(highFreqOperator)
+            .lowFreqOperator(lowFreqOperator)
+            .minimumShouldMatch(minimumShouldMatch)
+            .query(query);
     }
 
     // ---------------------------------------------------------------------------------------------
