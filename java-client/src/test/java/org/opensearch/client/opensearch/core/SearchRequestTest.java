b7a67509af8cf43b0d4a0106d01ece7cfbe75529
channel-dante <dante@channel.io>
2024-01-03 14:15:52 +0900

    removed not related test code
    Signed-off-by: channel-dante <dante@channel.io>

--- java-client/src/test/java/org/opensearch/client/opensearch/core/SearchRequestTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/core/SearchRequestTest.java
@@ -1,16 +1,9 @@
 package org.opensearch.client.opensearch.core;
 
 import org.junit.Test;
-import org.opensearch.client.opensearch._types.FieldValue;
 import org.opensearch.client.opensearch.model.ModelTestCase;
 
 public class SearchRequestTest extends ModelTestCase {
-    @Test
-    public void afterSearch() {
-        SearchRequest request = new SearchRequest.Builder().searchAfter(FieldValue.of(1), FieldValue.of("string")).build();
-
-        assertEquals("{\"search_after\":[1,\"string\"]}", toJson(request));
-    }
 
     @Test
     public void toBuilder() {
