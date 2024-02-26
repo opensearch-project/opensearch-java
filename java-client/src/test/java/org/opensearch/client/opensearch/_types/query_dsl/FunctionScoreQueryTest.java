640fce0717e4532ac76f22d956f6f88970cb1239
Russ Cam <russcam@canva.com>
2024-02-26 18:40:34 +1000

    Support optional function score variant
    This commit supports function score functions with an
    optional function variant. The function is optional as it
    is perfectly valid to have a function with only a weight e.g.
    a function score query with multiple functions and a score mode
    of "first", where the last weight function acts as some default value.
    
    Closes opensearch-project/opensearch-java#820
    
    Signed-off-by: Russ Cam <russcam@canva.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/FunctionScoreQueryTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/FunctionScoreQueryTest.java
@@ -11,4 +11,37 @@ public class FunctionScoreQueryTest extends ModelTestCase {
 
         assertEquals(toJson(copied), toJson(origin));
     }
+
+    @Test
+    public void canSupportWeightFunction() {
+        Query functionScoreQuery = FunctionScoreQuery.of(fs -> fs
+                .functions(f -> f.weight(5d))
+        )._toQuery();
+
+        String json = "{\"function_score\":{\"functions\":[{\"weight\":5.0}]}}";
+
+        assertEquals(json, toJson(functionScoreQuery));
+
+        Query roundtripQuery = checkJsonRoundtrip(functionScoreQuery, json);
+
+        assertNull(roundtripQuery.functionScore().functions().get(0)._kind());
+        assertEquals(5.0, roundtripQuery.functionScore().functions().get(0).weight(), 0.001);
+    }
+
+    @Test
+    public void canSupportFunctionVariant() {
+        Query functionScoreQuery = FunctionScoreQuery.of(fs -> fs
+                .functions(f -> f.weight(3d).linear(l -> l.field("field").placement(p -> p.decay(8.0))))
+        )._toQuery();
+
+        String json = "{\"function_score\":{\"functions\":[{\"weight\":3.0,\"linear\":{\"field\":{\"decay\":8.0}}}]}}";
+
+        assertEquals(json, toJson(functionScoreQuery));
+
+        Query roundtripQuery = checkJsonRoundtrip(functionScoreQuery, json);
+
+        assertEquals(FunctionScore.Kind.Linear, roundtripQuery.functionScore().functions().get(0)._kind());
+        assertEquals(3.0, roundtripQuery.functionScore().functions().get(0).weight(), 0.001);
+        assertEquals(8.0, roundtripQuery.functionScore().functions().get(0).linear().placement().decay(), 0.001);
+    }
 }
