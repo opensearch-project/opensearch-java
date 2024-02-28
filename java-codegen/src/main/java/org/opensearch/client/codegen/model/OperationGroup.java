32c3d2ad10f49e9ccb41a104695931808be28d61
Thomas Farr <tsfarr@amazon.com>
2024-02-28 13:45:49 +1300

    Change OperationGroup matching logic to determine which to generate
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- java-codegen/src/main/java/org/opensearch/client/codegen/model/OperationGroup.java
+++ java-codegen/src/main/java/org/opensearch/client/codegen/model/OperationGroup.java
@@ -1,5 +1,9 @@
 package org.opensearch.client.codegen.model;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -47,5 +51,55 @@ public class OperationGroup {
     @Override
     public int hashCode() {
         return new HashCodeBuilder(17, 37).append(namespace).append(name).toHashCode();
+    }
+
+    public static Matcher matcher() {
+        return new Matcher();
+    }
+
+    public static class Matcher {
+        private final Set<String> namespaces = new HashSet<>();
+        private final Set<OperationGroup> operations = new HashSet<>();
+        private final Collection<Pattern> patterns = new HashSet<>();
+
+        private Matcher() {
+        }
+
+        public Matcher add(String namespace, String... operations) {
+            if (operations == null || operations.length == 0) {
+                namespaces.add(namespace);
+            } else {
+                for (String operation : operations) {
+                    add(new OperationGroup(namespace, operation));
+                }
+            }
+            return this;
+        }
+
+        public Matcher add(OperationGroup operation) {
+            operations.add(operation);
+            return this;
+        }
+
+        public Matcher add(Pattern pattern) {
+            patterns.add(pattern);
+            return this;
+        }
+
+        public boolean matches(OperationGroup operation) {
+            if (namespaces.contains(operation.getNamespace())) {
+                return true;
+            }
+            if (operations.contains(operation)) {
+                return true;
+            }
+            var str = operation.toString();
+            for (Pattern pattern : patterns) {
+                if (pattern.matcher(str).matches()) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
