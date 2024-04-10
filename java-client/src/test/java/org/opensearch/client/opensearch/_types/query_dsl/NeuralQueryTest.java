f6d38e52a3a97b44353e7a4910c824662e6369cb
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2024-04-10 15:54:21 +0200

    :green_heart: spotless fix
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
@@ -1,14 +1,17 @@
 package org.opensearch.client.opensearch._types.query_dsl;
 
+import java.util.List;
 import org.junit.Test;
 import org.opensearch.client.opensearch.model.ModelTestCase;
-
-import java.util.List;
 
 public class NeuralQueryTest extends ModelTestCase {
     @Test
     public void toBuilder() {
-        NeuralQuery origin = new NeuralQuery.Builder().field("field").queryText("queryText").k(1).filter(IdsQuery.of(builder -> builder.values(List.of("Some_ID"))).toQuery()).build();
+        NeuralQuery origin = new NeuralQuery.Builder().field("field")
+            .queryText("queryText")
+            .k(1)
+            .filter(IdsQuery.of(builder -> builder.values(List.of("Some_ID"))).toQuery())
+            .build();
         NeuralQuery copied = origin.toBuilder().build();
 
         assertEquals(toJson(copied), toJson(origin));
