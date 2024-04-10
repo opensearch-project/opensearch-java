94b255050a97107af6b746fc0ccc6eda455f6fe1
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2024-04-10 15:41:59 +0200

    :adhesive_bandage: add filter to neural query
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    (cherry picked from commit e964c6a5930219b729978dec163f0ec120d1869f)

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
@@ -3,10 +3,12 @@ package org.opensearch.client.opensearch._types.query_dsl;
 import org.junit.Test;
 import org.opensearch.client.opensearch.model.ModelTestCase;
 
+import java.util.List;
+
 public class NeuralQueryTest extends ModelTestCase {
     @Test
     public void toBuilder() {
-        NeuralQuery origin = new NeuralQuery.Builder().field("field").queryText("queryText").k(1).build();
+        NeuralQuery origin = new NeuralQuery.Builder().field("field").queryText("queryText").k(1).filter(IdsQuery.of(builder -> builder.values(List.of("Some_ID"))).toQuery()).build();
         NeuralQuery copied = origin.toBuilder().build();
 
         assertEquals(toJson(copied), toJson(origin));
