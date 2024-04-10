bc786139815d43eba55f5e526dcae36fa14a95cd
Grogdunn <frzlollo@gmail.com>
2024-04-10 16:54:30 +0200

    Add "filter" to neural query (#932)
    * :adhesive_bandage: add filter to neural query
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    
    * CHANGELOG.md
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    
    * :green_heart: spotless fix
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    
    * :green_heart: spotless fix, jdk8 check
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    
    * :green_heart: spotless fix
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Andriy Redko <drreta@gmail.com>
    Signed-off-by: Grogdunn <frzlollo@gmail.com>
    
    ---------
    
    Signed-off-by: Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
    Signed-off-by: Grogdunn <frzlollo@gmail.com>
    Co-authored-by: Andriy Redko <drreta@gmail.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/NeuralQueryTest.java
@@ -6,7 +6,11 @@ import org.opensearch.client.opensearch.model.ModelTestCase;
 public class NeuralQueryTest extends ModelTestCase {
     @Test
     public void toBuilder() {
-        NeuralQuery origin = new NeuralQuery.Builder().field("field").queryText("queryText").k(1).build();
+        NeuralQuery origin = new NeuralQuery.Builder().field("field")
+            .queryText("queryText")
+            .k(1)
+            .filter(IdsQuery.of(builder -> builder.values("Some_ID")).toQuery())
+            .build();
         NeuralQuery copied = origin.toBuilder().build();
 
         assertEquals(toJson(copied), toJson(origin));
