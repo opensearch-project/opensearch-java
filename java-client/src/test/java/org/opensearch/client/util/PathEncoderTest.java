5ed94fbaa678c34512277e553f89b34513705e11
AntCode97 <dbswns97@gmail.com>
2024-05-24 23:42:00 +0900

    add pathEncoder test
    Signed-off-by: AntCode97 <dbswns97@gmail.com>

--- java-client/src/test/java/org/opensearch/client/util/PathEncoderTest.java
+++ java-client/src/test/java/org/opensearch/client/util/PathEncoderTest.java
@@ -1,8 +1,31 @@
 package org.opensearch.client.util;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class PathEncoderTest extends TestCase {
+import org.junit.Test;
 
-    public void testEncode() {}
+public class PathEncoderTest {
+
+    @Test
+    public void testEncode() {
+        // Test with a simple string
+        String simpleString = "test";
+        String encodedSimpleString = PathEncoder.encode(simpleString);
+        assertEquals(simpleString, encodedSimpleString);
+
+        // Test with a string that contains special characters
+        String specialString = "a/b";
+        String encodedSpecialString = PathEncoder.encode(specialString);
+        assertEquals("a%2Fb", encodedSpecialString);
+
+        // Test with a string that contains alphanumeric characters
+        String alphanumericString = "abc123";
+        String encodedAlphanumericString = PathEncoder.encode(alphanumericString);
+        assertEquals("abc123", encodedAlphanumericString);
+
+        // Test with a string that contains multiple segments
+        String multiSegmentString = "a/b/c/_refresh";
+        String encodedMultiSegmentString = PathEncoder.encode(multiSegmentString);
+        assertEquals("a%2Fb%2Fc%2F_refresh", encodedMultiSegmentString);
+    }
 }
