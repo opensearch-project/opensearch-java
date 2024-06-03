752df7be6d17e8fdcb674adf61cce0efea77bd99
miguel-vila <miguelvilag@gmail.com>
2024-06-03 12:06:40 -0500

    fix files headers
    Signed-off-by: miguel-vila <miguelvilag@gmail.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/ingest/TextEmbeddingProcessorTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/ingest/TextEmbeddingProcessorTest.java
@@ -1,3 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.client.opensearch.ingest;
 
 import java.util.Map;
