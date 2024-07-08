2d5f79c25645cf517f08cff77ebb061dfd0890d1
Andriy Redko <andriy.redko@aiven.io>
2024-07-08 10:03:54 -0400

    Bump org.owasp.dependencycheck from 9.2.0 to 10.0.2 (#1069) (#1071)
    * Bump org.owasp.dependencycheck from 9.2.0 to 10.0.2
    
    Bumps org.owasp.dependencycheck from 9.2.0 to 10.0.2.
    
    ---
    updated-dependencies:
    - dependency-name: org.owasp.dependencycheck
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update changelog
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <dependabot[bot]@users.noreply.github.com>
    (cherry picked from commit 848be31bf9c56f8715da17586028f1d01f50fd7d)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -48,8 +48,8 @@ plugins {
     java
     `java-library`
     `maven-publish`
-    id("com.github.jk1.dependency-license-report") version "2.7"
-    id("org.owasp.dependencycheck") version "9.1.0"
+    id("com.github.jk1.dependency-license-report") version "2.8"
+    id("org.owasp.dependencycheck") version "10.0.2"
     id("com.diffplug.spotless") version "6.25.0"
 }
 
