6b6a988b692da9c0505567c58581ec0b693ad240
Akanksha Agrawal <akanksha.agrawal@walmart.com>
2024-07-25 15:41:00 +0530

    dummy commit

--- java-client/src/main/java/org/opensearch/client/transport/httpclient5/ApacheHttpClient5Transport.java
+++ java-client/src/main/java/org/opensearch/client/transport/httpclient5/ApacheHttpClient5Transport.java
@@ -188,7 +188,7 @@ public class ApacheHttpClient5Transport implements OpenSearchTransport {
 
         return future.thenApply(r -> {
             try {
-                return (ResponseT) prepareResponse(r, endpoint);
+                return prepareResponse(r, endpoint);
             } catch (final IOException ex) {
                 throw new CompletionException(ex);
             }
