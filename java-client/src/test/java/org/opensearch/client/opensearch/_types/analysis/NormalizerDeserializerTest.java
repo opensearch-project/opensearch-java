6cb4e10acd589c849a6bdcff9bb27f0bf4817018
beniamin.jedrychowsk <beniamin.jedrychowski@transatel.com>
2024-07-25 18:09:24 +0200

    fix style
    Signed-off-by: beniamin.jedrychowsk <beniamin.jedrychowski@transatel.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/analysis/NormalizerDeserializerTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/analysis/NormalizerDeserializerTest.java
@@ -12,11 +12,10 @@ import org.junit.Test;
 import org.opensearch.client.opensearch.model.ModelTestCase;
 
 public class NormalizerDeserializerTest extends ModelTestCase {
+
     @Test
     public void deserializesTypelessCustomAnalyzer() {
-        String json = "{\n" +
-            "          \"filter\": \"lowercase\"\n" +
-            "      }";
+        String json = "{\n" + "          \"filter\": \"lowercase\"\n" + "      }";
 
         Normalizer normalizer = fromJson(json, Normalizer._DESERIALIZER);
         assertTrue(normalizer.isCustom());
