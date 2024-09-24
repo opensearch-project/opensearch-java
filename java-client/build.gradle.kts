a5288c394d383e37feb43d7932fb4709a19d4437
Andriy Redko <andriy.redko@aiven.io>
2024-09-23 22:53:10 -0400

    Bump org.apache.httpcomponents.client5:httpclient5 from 5.3.1 to 5.4 (#1204) (#1206)
    * Bump org.apache.httpcomponents.client5:httpclient5 from 5.3.1 to 5.4
    
    Bumps [org.apache.httpcomponents.client5:httpclient5](https://github.com/apache/httpcomponents-client) from 5.3.1 to 5.4.
    - [Changelog](https://github.com/apache/httpcomponents-client/blob/master/RELEASE_NOTES.txt)
    - [Commits](https://github.com/apache/httpcomponents-client/compare/rel/v5.3.1...rel/v5.4)
    
    ---
    updated-dependencies:
    - dependency-name: org.apache.httpcomponents.client5:httpclient5
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    
    
    * Update changelog
    
    
    
    ---------
    
    
    
    
    (cherry picked from commit 888121822d2fe05159aa03a1708dec2f40b15bff)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <dependabot[bot]@users.noreply.github.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -206,7 +206,7 @@ dependencies {
     testImplementation("com.fasterxml.jackson.datatype", "jackson-datatype-jakarta-jsonp", jacksonVersion)
 
     // ApacheHttpClient5Transport dependencies (optional)
-    implementation("org.apache.httpcomponents.client5", "httpclient5", "5.3.1") {
+    implementation("org.apache.httpcomponents.client5", "httpclient5", "5.4") {
       exclude(group = "org.apache.httpcomponents.core5")
     }
     implementation("org.apache.httpcomponents.core5", "httpcore5", "5.3")
