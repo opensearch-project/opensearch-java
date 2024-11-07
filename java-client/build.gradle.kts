cc76872831bd99323fe7959649d9fae5b969e60a
Thomas Farr <tsfarr@amazon.com>
2024-11-08 00:29:01 +1300

    Fix
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -190,7 +190,7 @@ dependencies {
       exclude(group = "org.apache.httpcomponents.core5")
     }
     api("org.apache.httpcomponents.core5:httpcore5:5.3.1")
-    api("org.apache.httpcomponents.core5:httpcore5-h2:5.3.1.1")
+    api("org.apache.httpcomponents.core5:httpcore5-h2:5.3.1")
 
     // Apache 2.0
     // https://search.maven.org/artifact/com.google.code.findbugs/jsr305
