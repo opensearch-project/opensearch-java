dc8322cf414060acbefad8b64edff5ed6eef6441
Thomas Farr <tsfarr@amazon.com>
2025-01-06 11:37:42 +1300

    Generate ingest.put_pipeline
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- java-client/src/generated/java/org/opensearch/client/opensearch/ingest/PutPipelineResponse.java
+++ java-client/src/generated/java/org/opensearch/client/opensearch/ingest/PutPipelineResponse.java
@@ -30,39 +30,80 @@
  * GitHub history for details.
  */
 
+//----------------------------------------------------
+// THIS CODE IS GENERATED. MANUAL EDITS WILL BE LOST.
+//----------------------------------------------------
+
 package org.opensearch.client.opensearch.ingest;
 
 import java.util.function.Function;
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
 import org.opensearch.client.json.JsonpDeserializable;
 import org.opensearch.client.json.JsonpDeserializer;
 import org.opensearch.client.json.ObjectBuilderDeserializer;
 import org.opensearch.client.json.ObjectDeserializer;
 import org.opensearch.client.opensearch._types.AcknowledgedResponseBase;
+import org.opensearch.client.util.CopyableBuilder;
 import org.opensearch.client.util.ObjectBuilder;
+import org.opensearch.client.util.ToCopyableBuilder;
 
 // typedef: ingest.put_pipeline.Response
 
 @JsonpDeserializable
-public class PutPipelineResponse extends AcknowledgedResponseBase {
+@Generated("org.opensearch.client.codegen.CodeGenerator")
+public class PutPipelineResponse extends AcknowledgedResponseBase
+    implements
+        ToCopyableBuilder<PutPipelineResponse.Builder, PutPipelineResponse> {
+
     // ---------------------------------------------------------------------------------------------
 
     private PutPipelineResponse(Builder builder) {
         super(builder);
-
     }
 
-    public static PutPipelineResponse of(Function<Builder, ObjectBuilder<PutPipelineResponse>> fn) {
+    public static PutPipelineResponse of(Function<PutPipelineResponse.Builder, ObjectBuilder<PutPipelineResponse>> fn) {
         return fn.apply(new Builder()).build();
     }
 
     // ---------------------------------------------------------------------------------------------
 
+    @Override
+    @Nonnull
+    public Builder toBuilder() {
+        return new Builder(this);
+    }
+
+    @Nonnull
+    public static Builder builder() {
+        return new Builder();
+    }
+
     /**
      * Builder for {@link PutPipelineResponse}.
      */
+    public static class Builder extends AcknowledgedResponseBase.AbstractBuilder<Builder>
+        implements
+            CopyableBuilder<Builder, PutPipelineResponse> {
 
-    public static class Builder extends AcknowledgedResponseBase.AbstractBuilder<Builder> implements ObjectBuilder<PutPipelineResponse> {
+        public Builder() {}
+
+        private Builder(PutPipelineResponse o) {
+            super(o);
+        }
+
+        private Builder(Builder o) {
+            super(o);
+        }
+
         @Override
+        @Nonnull
+        public Builder copy() {
+            return new Builder(this);
+        }
+
+        @Override
+        @Nonnull
         protected Builder self() {
             return this;
         }
@@ -70,9 +111,10 @@ public class PutPipelineResponse extends AcknowledgedResponseBase {
         /**
          * Builds a {@link PutPipelineResponse}.
          *
-         * @throws NullPointerException
-         *             if some of the required fields are null.
+         * @throws NullPointerException if some of the required fields are null.
          */
+        @Override
+        @Nonnull
         public PutPipelineResponse build() {
             _checkSingleUse();
 
@@ -91,8 +133,22 @@ public class PutPipelineResponse extends AcknowledgedResponseBase {
     );
 
     protected static void setupPutPipelineResponseDeserializer(ObjectDeserializer<PutPipelineResponse.Builder> op) {
-        AcknowledgedResponseBase.setupAcknowledgedResponseBaseDeserializer(op);
-
+        setupAcknowledgedResponseBaseDeserializer(op);
     }
 
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) {
+            return false;
+        }
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        return true;
+    }
 }
