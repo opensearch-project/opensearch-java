f6570a3eaac2d157e45c9ca95ea72424cb88f526
Thomas Farr <tsfarr@amazon.com>
2025-02-13 15:48:08 +1300

    Add support for `GeoDistanceQuery`'s `ignore_unmapped` property
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/GeoDistanceQueryTest.java
+++ java-client/src/test/java/org/opensearch/client/opensearch/_types/query_dsl/GeoDistanceQueryTest.java
@@ -10,6 +10,7 @@ package org.opensearch.client.opensearch._types.query_dsl;
 
 import java.util.Collections;
 import org.junit.Test;
+import org.opensearch.client.opensearch._types.GeoDistanceType;
 import org.opensearch.client.opensearch._types.GeoLocation;
 import org.opensearch.client.opensearch.model.ModelTestCase;
 
@@ -18,6 +19,9 @@ public class GeoDistanceQueryTest extends ModelTestCase {
     public void toBuilder() {
         GeoDistanceQuery origin = new GeoDistanceQuery.Builder().field("field")
             .location(new GeoLocation.Builder().coords(Collections.singletonList(1.0)).build())
+            .distance("1m")
+            .distanceType(GeoDistanceType.Arc)
+            .ignoreUnmapped(true)
             .build();
         GeoDistanceQuery copied = origin.toBuilder().build();
 
