8194c475950f66ae6e62e1ea6d12bff09d8ac94c
psingh3 <pushpendra.singh@walmart.com>
2025-03-19 00:19:13 +0530

    enabled tests.rest.cluster property

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -130,7 +130,6 @@ tasks.test {
 
     systemProperty("tests.security.manager", "false")
     // Basic auth settings for integration test
-    systemProperty("tests.rest.cluster", System.getProperty("tests.rest.cluster", null))
     systemProperty("https", System.getProperty("https", "true"))
     systemProperty("user", System.getProperty("user", "admin"))
     systemProperty("password", System.getProperty("password", "admin"))
@@ -148,6 +147,7 @@ val integrationTest = task<Test>("integrationTest") {
     }
     systemProperty("tests.security.manager", "false")
     // Basic auth settings for integration test
+    systemProperty("tests.rest.cluster", System.getProperty("tests.rest.cluster", null))
     systemProperty("https", System.getProperty("https", "true"))
     systemProperty("user", System.getProperty("user", "admin"))
     systemProperty("password", System.getProperty("password", "admin"))
