c009e25680c2c13bfbb7135653743b20d8687be7
psingh3 <pushpendra.singh@walmart.com>
2025-03-18 10:18:09 +0530

    Added empty check and enabled tests.rest.cluster prop

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -130,6 +130,7 @@ tasks.test {
 
     systemProperty("tests.security.manager", "false")
     // Basic auth settings for integration test
+    systemProperty("tests.rest.cluster", System.getProperty("tests.rest.cluster", null))
     systemProperty("https", System.getProperty("https", "true"))
     systemProperty("user", System.getProperty("user", "admin"))
     systemProperty("password", System.getProperty("password", "admin"))
