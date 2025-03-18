4ba08c295ed2382ee2455e0d309b41f2371bbb17
psingh3 <pushpendra.singh@walmart.com>
2025-03-18 15:50:20 +0530

    added ClientMetricsIT

---
+++ java-client/src/test/java11/org/opensearch/client/opensearch/integTest/httpclient5/ClientMetricsIT.java
@@ -0,0 +1,5 @@
+package org.opensearch.client.opensearch.integTest.httpclient5;
+
+import org.opensearch.client.opensearch.integTest.AbstractClientMetricsIT;
+
+public class ClientMetricsIT extends AbstractClientMetricsIT implements HttpClient5TransportSupport {}
